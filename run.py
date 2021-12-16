29023e2d80b9e85d016a9391adf609c6dd0973a2
YuCheng Chang <71141033+nonoivan1234@users.noreply.github.com>
2021-12-17 03:08:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,14 +14,17 @@ url = 'https://www.sssh.tp.edu.tw/'
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
 driver.get(url)
 
-time.sleep(2)
+time.sleep(3)
 i = 1
 
 while True:
-    print('progressing', i)
-    btn = driver.find_element('xpath', '//a[@nid="21369"]')
-    click(btn)
-    close = driver.find_element('xpath', '//*[@id="layout"]/div[4]/a')
-    click(close)
-    print(i, 'complete')
-    i += 1
+    try:
+        print('progressing', i)
+        btn = driver.find_element('xpath', '//a[@nid="21369"]')
+        click(btn)
+        close = driver.find_element('xpath', '//*[@id="layout"]/div[4]/a')
+        click(close)
+        print(i, 'complete')
+        i += 1
+    except:
+        print('FAILED', i)
