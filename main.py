80bb1c9de77f667f8105ebeafc38b0402f79ee6d
YuCheng Chang <71141033+nonoivan1234@users.noreply.github.com>
2021-12-17 02:55:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def click(target):
     ActionChains(edge).move_to_element(target).perform()
     ActionChains(edge).click(target).perform()
 
-
+print('run main.py')
 url = 'https://www.sssh.tp.edu.tw/'
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
@@ -17,6 +17,7 @@ time.sleep(2)
 i = 1
 
 while True:
+    print('progressing', i)
     btn = driver.find_element('xpath', '//a[@nid="21369"]')
     click(btn)
     close = driver.find_element('xpath', '//*[@id="layout"]/div[4]/a')
