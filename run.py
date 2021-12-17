cf642a25d261093d81bd12b3bec30ca7b5bb4b7a
YuCheng Chang <71141033+nonoivan1234@users.noreply.github.com>
2021-12-17 13:54:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 import os
 import time
 
@@ -8,25 +9,34 @@ def click(target):
     ActionChains(driver).move_to_element(target).perform()
     ActionChains(driver).click(target).perform()
 
-print('run main.py')
 url = 'https://www.sssh.tp.edu.tw/'
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 
-time.sleep(3)
+time.sleep(2)
 i = 1
-
-while True:
+while (i<500):
     try:
         print('progressing', i)
         btn = driver.find_element('xpath', '//a[@nid="21369"]')
         # print('driver find btn', i)
         click(btn)
+        print(i, 'complete')
         close = driver.find_element('xpath', '//*[@id="layout"]/div[4]/a')
         # print('driver find close', i)
         click(close)
-        print(i, 'complete')
         i += 1
+        
     except:
-        print('FAILED', i)
+        print('FAILED click close', i)
+        i += 1
+        driver.get(url)
+        time.sleep(2)
+print('Finished 500 times')
+quit()
